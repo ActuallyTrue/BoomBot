e86f45c8d166e1ac89b8b8fc42fd57c78f5ca08b
Fattimo <matt.chen522@gmail.com>
2022-02-17 20:06:01 -0500

    memo1/commit2

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hello!");
+        print("hi world!");
     }
 
     // Update is called once per frame
