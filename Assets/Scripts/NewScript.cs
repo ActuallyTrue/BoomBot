c583c034824fd380065d816eeebaabdbc8b934e4
Sukriti Bhardwaj <sukriti1.bhardwaj@gmail.com>
2022-02-19 01:21:59 -0500

    update

--- Assets/Scripts/NewScript.cs
+++ Assets/Scripts/NewScript.cs
@@ -7,7 +7,7 @@ public class NewScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hi world");
+        print("BoomBot says hello");
     }
 
     // Update is called once per frame
