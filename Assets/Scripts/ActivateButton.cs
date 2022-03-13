8312887dd68ffa5975f5f9518627381013f6b3e3
ActuallyTrue <dotaigbe@icloud.com>
2022-03-13 17:03:19 -0400

    Fixed the door sound problem

--- Assets/Scripts/ActivateButton.cs
+++ Assets/Scripts/ActivateButton.cs
@@ -7,6 +7,8 @@ public class ActivateButton : MonoBehaviour
     private bool isActivatedEnter = false;
     private bool isActivatedExit = false;
 
+    public bool pressed = false;
+
     public bool getActivatedEnter() {
         return isActivatedEnter;
     }
@@ -18,27 +20,31 @@ public class ActivateButton : MonoBehaviour
     public void setActivatedEnter() {
         isActivatedEnter = false;
         EventManager.TriggerEvent<Vector3>("buttonClickAudio", this.transform.position);
+        EventManager.TriggerEvent<Vector3>("doorOpenAudio", this.transform.position);
     }
 
     public void setActivatedExit() {
         isActivatedExit = false;
         EventManager.TriggerEvent<Vector3>("buttonClickAudio", this.transform.position);
+        EventManager.TriggerEvent<Vector3>("doorCloseAudio", this.transform.position);
     }
 
-    // void OnTriggerEnter(Collider c) {
-    //     if (c.attachedRigidbody != null) {
-    //         CanActivateButton collidingObject = c.attachedRigidbody.gameObject.GetComponent<CanActivateButton>();
-    //         if (collidingObject != null) {
-    //             isActivatedEnter = true;
-    //         }
-    //     }
-    // }
+    void OnTriggerEnter(Collider c) {
+        if (c.attachedRigidbody != null) {
+            CanActivateButton collidingObject = c.attachedRigidbody.gameObject.GetComponent<CanActivateButton>();
+            if (collidingObject != null) {
+                EventManager.TriggerEvent<Vector3>("buttonClickAudio", this.transform.position);
+                EventManager.TriggerEvent<Vector3>("doorOpenAudio", this.transform.position);
+            }
+        }
+    }
 
     private void OnTriggerStay(Collider other) {
         if (other.attachedRigidbody != null) {
             CanActivateButton collidingObject = other.attachedRigidbody.gameObject.GetComponent<CanActivateButton>();
             if (collidingObject != null) {
                 isActivatedEnter = true;
+                pressed = true;
             }
         }
     }
@@ -48,6 +54,9 @@ public class ActivateButton : MonoBehaviour
             CanActivateButton collidingObject = c.attachedRigidbody.gameObject.GetComponent<CanActivateButton>();
             if (collidingObject != null) {
                 isActivatedExit = true;
+                pressed = false;
+                EventManager.TriggerEvent<Vector3>("buttonClickAudio", this.transform.position);
+            EventManager.TriggerEvent<Vector3>("doorCloseAudio", this.transform.position);
             }
         }
     }
--- Assets/Scripts/OpenDoorButton.cs
+++ Assets/Scripts/OpenDoorButton.cs
@@ -6,7 +6,7 @@ public class OpenDoorButton : MonoBehaviour
 {
     public GameObject buttons;
 
-    private Animator anim;
+    public Animator anim;
     private bool alreadyEntered = false;
     private bool alreadyExited = false;
 
@@ -20,25 +20,41 @@ public class OpenDoorButton : MonoBehaviour
     void Update()
     {
         ActivateButton button = (ActivateButton) buttons.GetComponent(typeof(ActivateButton));
-        if (button.getActivatedEnter() && !alreadyEntered) {
-            print("here!");
-            alreadyEntered = true;
-            anim.SetBool("open", true);
-            anim.SetBool("close", false);
-            button.setActivatedEnter();
-            EventManager.TriggerEvent<Vector3>("doorOpenAudio", this.transform.position);
-        } else {
-            alreadyEntered = false;
+        if (button.pressed)
+        {
+            if (anim != null)
+            {
+                anim.SetBool("open", true);
+                anim.SetBool("close", false);
+            }
+            
+        } else{
+            if (anim != null)
+            {
+                anim.SetBool("open", false);
+                anim.SetBool("close", true);
+            }
+            
         }
-        if (button.getActivatedExit() && !alreadyExited) {
-            alreadyExited = true;
-            anim.SetBool("open", false);
-            anim.SetBool("close", true);
-            button.setActivatedExit();
-            EventManager.TriggerEvent<Vector3>("doorCloseAudio", this.transform.position);
-        }
-        else {
-            alreadyExited = false;
-        }
+        // if (button.getActivatedEnter() && !alreadyEntered) {
+        //     //print(alreadyEntered);
+        //     alreadyEntered = true;
+            
+        //     button.setActivatedEnter();
+            
+        // } else {
+        //     alreadyEntered = false;
+        // }
+        // print(alreadyEntered);
+        // if (button.getActivatedExit() && !alreadyExited) {
+        //     alreadyExited = true;
+        //     anim.SetBool("open", false);
+        //     anim.SetBool("close", true);
+        //     button.setActivatedExit();
+            
+        // }
+        // else {
+        //     alreadyExited = false;
+        // }
     }
 }
