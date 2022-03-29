a8389a555e4acd6be32fd1f9f2014f4327372f3f
Heather Zhu <heatherzhu17@outlook.com>
2022-03-29 16:54:36 -0400

    Level3 Update
    Completed
    - Added 4 circular saws that move back and forth in the room that knock the player back when collided with.
    
    TODO:
    - Add door functionality (when the player steps on all 4 buttons, the door opens)

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    private health playerHealth;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = FindObjectOfType<health>();
+        player = GameObject.Find("Boombot");
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.CompareTag("Player")) {
+            player.GetComponent<Rigidbody>().AddForce(player.transform.forward * -15f,ForceMode.Impulse);
+        } 
+    }
+}
