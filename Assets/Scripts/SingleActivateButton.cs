795c67ee7edaaff11cf68fbe4351b0d974949d0d
Heather Zhu <heatherzhu17@outlook.com>
2022-03-31 19:45:29 -0400

    Finish Level3 + playability adjustments

--- Assets/Scripts/SingleActivateButton.cs
+++ Assets/Scripts/SingleActivateButton.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SingleActivateButton : MonoBehaviour
 {
+
+    public bool isActivated;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isActivated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if (c.gameObject.CompareTag("Player")) {
+            if (!isActivated) {
+                EventManager.TriggerEvent<Vector3>("buttonClickAudio", this.transform.position);
+                isActivated = true;
+            }
+           
+        }
     }
 }
--- Assets/Scripts/SingleActivateDoor.cs
+++ Assets/Scripts/SingleActivateDoor.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SingleActivateDoor : MonoBehaviour
 {
+    public GameObject[] buttonList;
+    public Animator anim;
+
+    private bool canOpen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canOpen = false;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int count = 0;
+        for (int i = 0; i < buttonList.Length; i++) {
+            SingleActivateButton button = (SingleActivateButton) buttonList[i].GetComponent(typeof(SingleActivateButton));
+            if (button.isActivated) {
+                count++;
+            }
+        }
+
+        if (count == buttonList.Length) {
+            if (anim != null && !anim.GetBool("open")) {
+                anim.SetBool("open", true);
+                anim.SetBool("close", false);
+                EventManager.TriggerEvent<Vector3>("doorOpenAudio", this.transform.position);
+            }
+            
+        }
     }
 }
